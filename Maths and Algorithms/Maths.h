29653b59d8d1be40e10a4091838c904f70a1a2f1
Tommy <s287304@uos.ac.uk>
2025-04-29 12:22:01 +0100

    Matrix
    Determinate

--- Maths and Algorithms/Maths.h
+++ Maths and Algorithms/Maths.h
@@ -1,6 +1,6 @@
 #pragma once
 #define _USE_MATH_DEFINES
-#include <math.h>
+#include "math.h"
 #include <cmath>
 
 namespace MathUtils
--- Maths and Algorithms/Matrix.cpp
+++ Maths and Algorithms/Matrix.cpp
@@ -1,1 +1,51 @@
 #include "Matrix.h"
+
+
+// 1  2  3 
+// 4  5  6
+// 7  8  9
+
+ float Matrix::Determinate(){
+
+	 float D = 0;
+	  
+	 float x = Data[0][0] * ((Data[1][1] * Data[2][2]) - (Data[1][2] * Data[2][1]));
+
+	 float y = Data[1][0] * ((Data[1][0] * Data[2][2]) - (Data[1][2] * Data[2][0]));
+
+	 float z = Data[0][2] * ((Data[1][0] * Data[2][1]) - (Data[1][1] * Data[2][0]));
+
+	 D = x - y + z;
+
+	 return D;
+
+}
+
+void Matrix::SetRotateAroundX(float Angle) {
+
+
+
+}
+void Matrix::SetRotateAroundY(float Angle) {
+
+
+
+}
+void Matrix::SetRotateAroundZ(float Angle) {
+
+
+
+}
+
+void Matrix::Translate(Vector delta) {
+
+
+}
+ void Matrix::SetMatrix(int[3][3]) {
+
+	float D = 0;
+
+	int matrix[3][3] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+
+
+}
