82de7adb4a128de21ab26298ca7e5e1d04d5b729
Tommy <s287304@uos.ac.uk>
2025-05-13 12:06:47 +0100

    Seach  algorithmns

--- Maths and Algorithms/Algorithms.h
+++ Maths and Algorithms/Algorithms.h
@@ -1,9 +1,10 @@
 #pragma once
+#include <iostream>
 namespace Algorithms
 {
     inline int * BubbleSort(int* Data, size_t Size)
 	 {
-		 return nullptr;
+		 
 	 }
 
 
@@ -43,15 +44,69 @@ namespace Algorithms
 	 }
 
 
-    inline int * LinierSearch(int* Data, size_t Size, int target)
+    inline bool LinierSearch(int* Data, size_t Size, int target)
 	 {
-		 return nullptr;
+
+		for (int i = 0; i < Size; i++) {
+
+			if (Data[i] == target) {
+
+				return true;
+			}
+
+
+		}
+		return false;
+
 	 }
 
 
-    inline int * BinarySearch(int* Data, size_t Size, int target)
+    inline bool BinarySearch(int* Data, size_t Size, int target)
 	 {
-		 return nullptr;
+		int Midpoint = (Size -1) / 2;
+		int Endpoint = Size;
+
+
+		if (target == Data[Midpoint])
+				return true; //stop the loop
+		if (target < Data[Midpoint])
+		{
+			int* sub = new int[Midpoint];
+			for (int j = 0; j < Midpoint; j++)
+			{
+				sub[j] = Data[j];
+			}
+			BinarySearch(sub, Midpoint, target);
+		}
+		if (target > Data[Midpoint]) {
+
+			int* sub = new int[Midpoint];
+			for (int k = Midpoint; k < Endpoint; k++)
+			{
+				sub[k] = Data[k];
+			}
+			BinarySearch(sub, Midpoint, target);
+
+
+			}
+			S
+
+
+
+		// while( i >1 )
+		// {
+		//	
+		//	i= (Size - 1) / 2;
+
+		//	if (target == Data[i])
+		//		break; //stop the loop
+		//	if(target < Data[i])
+		//		//do left
+		//	if(target > Data[i])
+		//		//do right
+		//
+		//}
+		 
 	 }
 }
 
